935474c08b97e238e98fb56da41e21bd66084893
Samuele Angelicchio <samueleangelicchio9@gmail.com>
2025-09-30 13:12:16 +0200

    fix and implementation

--- src/app/components/OnePage/About.tsx
+++ src/app/components/OnePage/About.tsx
@@ -1,17 +1,24 @@
 import { GoProjectRoadmap } from "react-icons/go";
 import { BiSolidCertification } from "react-icons/bi";
 import { FaPersonArrowUpFromLine } from "react-icons/fa6";
+import CounterCard from "../UI/CounterCard";
+
+const counters = [
+  { number: 1, label: "Progetti", Icon: GoProjectRoadmap },
+  { number: 2, label: "Certificati", Icon: BiSolidCertification },
+  { number: 1, label: "Anni Esperienza", Icon: FaPersonArrowUpFromLine },
+];
 
 export default function About() {
   return (
     <>
       <section
         id="about"
-        className="min-h-screen flex flex-col justify-center bg-slate-100 px-8 py-16"
+        className="min-h-screen flex flex-col justify-center bg-gradient-to-r from-white to-gray-200  px-8 py-16"
       >
         <div className="container mx-auto text-center max-w-3xl">
           <h1 className="text-5xl font-bold mb-6">About Me</h1>
-          <p className="text-gray-600 leading-relaxed mb-8 text-xl">
+          <p className="text-gray-600 leading-relaxed mb-8 text-2xl">
             Fin da piccolo sono sempre stato affascinato dalla tecnologia e
             dall'informatica. All’inizio ho fatto qualche passo falso, e dopo
             anni di lavori diversi ho deciso finalmente di seguire la mia
@@ -19,30 +26,20 @@ export default function About() {
             inseguire i miei sogni, con l’unico obiettivo di crescere e
             migliorare ogni giorno.
           </p>
-          <a
-            href=""
-            className="inline-block bg-verdigris text-white font-medium px-6 py-3 rounded-lg shadow hover:bg-redCrayola hover:text-night transition"
-          >
-            Download CV
+          <a href="">
+            <button className="relative py-2 px-8 text-black text-base font-bold nded-full overflow-hidden bg-white rounded-full transition-all duration-400 ease-in-out shadow-md hover:scale-105 hover:text-white hover:shadow-lg active:scale-90 before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-redCrayola before:to-red-300 before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-full hover:before:left-0">
+              Download CV
+            </button>
           </a>
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 mt-16">
-            <div className="bg-gray-100 p-6 rounded-lg shadow">
-              <h2 className="text-4xl font-bold text-verdigris">1</h2>
-              <p className="mt-2 text-gray-500 font-medium">Progetti</p>
-              <GoProjectRoadmap className="text-5xl mx-auto" />
-            </div>
-
-            <div className="bg-gray-100 p-6 rounded-lg shadow">
-              <h2 className="text-4xl font-bold text-verdigris">2</h2>
-              <p className="mt-2 text-gray-500 font-medium">Certificati</p>
-              <BiSolidCertification className="text-5xl mx-auto" />
-            </div>
-
-            <div className="bg-gray-100 p-6 rounded-lg shadow">
-              <h2 className="text-4xl font-bold text-verdigris">1</h2>
-              <p className="mt-2 text-gray-500 font-medium">Anni Esperienza</p>
-              <FaPersonArrowUpFromLine className="text-5xl mx-auto" />
-            </div>
+            {counters.map((counters, idx) => (
+              <CounterCard
+                key={idx}
+                number={counters.number}
+                label={counters.label}
+                Icon={counters.Icon}
+              />
+            ))}
           </div>
         </div>
       </section>
---
+++ src/app/components/UI/CounterCard.tsx
@@ -0,0 +1,17 @@
+interface CounterCardProps {
+  number: number | string;
+  label: string;
+  Icon: React.ElementType;
+}
+
+const CounterCard = ({ number, label, Icon }: CounterCardProps) => (
+  <div className="bg-gradient-to-r cursor-default from-slate-50 to-slate-200 px-8 py-6 rounded-lg shadow hover:bg-night hover:text-verdigris hover:scale-110 transform duration-300 hover:shadow-2xl">
+    <h2 className="text-4xl font-bold text-redCrayola">{number}</h2>
+    <p className="mt-2 bg-gradient-to-r from-slate-500 to-slate-900 bg-clip-text text-transparent font-medium">
+      {label}
+    </p>
+    <Icon className="text-5xl mx-auto" />
+  </div>
+);
+
+export default CounterCard;
