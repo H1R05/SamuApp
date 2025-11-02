ea2fe0086698e3d4051762fea7b1b124d6f1bf0d
Samuele Angelicchio <samueleangelicchio9@gmail.com>
2025-11-02 18:17:58 +0100

    update Loading section

--- src/app/components/UI/LoadingScreen.tsx
+++ src/app/components/UI/LoadingScreen.tsx
@@ -1,6 +1,6 @@
 export default function LoadingScreen() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-[#0b132b] z-[9999]">
+    <div className="fixed inset-0 flex items-center justify-center bg-black z-[9999]">
       <div className="w-10 h-10 border-4 border-yellow-400 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
